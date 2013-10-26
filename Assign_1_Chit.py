f89d467b3505c90389e617277f5e290de0b7d928
Lawrence Chit <lawchit@gmail.com>
2013-10-25 19:22:18 -0700

    Update Assign_1_Chit.py

--- Assign_1_Chit.py
+++ Assign_1_Chit.py
@@ -1,4 +1,5 @@
 # VIS 198 Assignment 1 - Lawrence Chit
+# NOT COMPLETE NOT COMPLETE NOT COMPLETE NOT COMPLETE NOT COMPLETE NOT COMPLETENOT COMPLETE NOT COMPLETE NOT COMPLETE
 
 import json
 import numpy
@@ -12,19 +13,24 @@ file4 = "/Users/AUNG/Documents/School/VIS 198/json4"
 json1_file = open(file1)
 json1_string = json1_file.read()
 json1_data = json.loads(json1_string)[0]
-print(json1_data['datapoints'][0], json1_data['target']) 
+print(json1_data['datapoints'][0] + " target: " + json1_data['target'])
+
 
 json2_file = open(file2)
 json2_string = json2_file.read()
 json2_data = json.loads(json2_string)[0]
-print(json2_data['datapoints'][0], json2_data['target'])
+print(json2_data['datapoints'][0] + " target: " + json2_data['target'])
 
 json3_file = open(file3)
 json3_string = json3_file.read()
 json3_data = json.loads(json3_string)[0]
-print(json3_data['datapoints'][0], json3_data['target'])
+print(json3_data['datapoints'][0] + " target: " + json3_data['target'])
 
 json4_file = open(file4)
 json4_string = json4_file.read()
 json4_data = json.loads(json4_string)[0]
-print(json4_data['datapoints'][0], json4_data['target'])
+print(json4_data['datapoints'][0] + " target: " + json4_data['target'])
+
+
+
+
